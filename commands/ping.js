4e6dec304ad86defacb356cb0c44eb7f6e5fb401
Just-a-Unity-Dev <just-a-unity-dev@users.noreply.github.com>
2022-06-29 22:14:38 +0800

    ping update

--- commands/ping.js
+++ commands/ping.js
@@ -4,6 +4,6 @@ module.exports = {
 		desc: "Ping pong!"
 	},
 	async execute(bot, msg, args) {
-		bot.createMessage(msg.channel.id, "Pong!");
+		bot.createMessage(msg.channel.id, `Pong.\nAPI ms: ${msg.timestamp - Date.now()}`);
 	},
 };
