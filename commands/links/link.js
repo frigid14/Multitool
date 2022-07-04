3b99562dad92dce0cd2d9ba984633bd216ef7367
Just-a-Unity-Dev <just-a-unity-dev@users.noreply.github.com>
2022-07-04 17:22:32 +0800

    config works

---
+++ commands/links/link.js
@@ -0,0 +1,20 @@
+const uConfig = require("./../../utils/config.js");
+
+module.exports = [
+	"link",
+	(msg, args) => {
+		if(args.length === 0) {
+			return "Invalid input";
+		}
+
+		const config = uConfig.getModule("links");
+
+		if (config === 1) return "An error occured.";
+		return config[args[0]];
+	},
+	{
+		description: "Make the bot say something",
+		fullDescription: "The bot will echo whatever is after the command label.",
+		usage: "<link_name>"
+	}
+]
--- utils/config.js
+++ utils/config.js
@@ -1,3 +1,4 @@
+const logger = require("./logging.js");
 const toml = require('toml');
 const path = require('path');
 const fs = require('fs');
@@ -8,13 +9,31 @@ function readFile(path) {
 
 function readConfig() {
 	const env = process.env.NODE_ENV || 'development';
+	let file = path.join(__dirname, "../config/main.toml")
 
 	if (env == 'development') {
-		return toml.parse(readFile(path.join(__dirname, "../config/test.toml")));
+		file = path.join(__dirname, "../config/test.toml");
 	}
-	return toml.parse(readFile(path.join(__dirname, "../config/main.toml")));
+
+	try {
+		return toml.parse(readFile(file));
+	} catch (e) {
+		logger.error("Parsing error on line " + e.line + ", column " + e.column + ": " + e.message);
+		return 1;
+	}
+}
+
+function getModule(module) {
+	const config = readConfig();
+
+	if (config["module"]) {
+		if (config.module[module]) {
+			return config.module[module]
+		} else { logger.error(`Specified module not found.`); return 1;}
+	} else { logger.error("No module set up."); return 1;}
 }
 
 module.exports = {
-	getConfig: readConfig
+	getConfig: readConfig,
+	getModule: getModule
 }
