31654e0401faf811df7e927a18e052bab6830da6
Just-a-Unity-Dev <just-a-unity-dev@users.noreply.github.com>
2022-07-04 16:34:21 +0800

    config util

--- utils/config.js
+++ utils/config.js
@@ -1,7 +1,21 @@
+const toml = require('toml');
+const path = require('path');
+const concat = require('concat-stream');
+const fs = require('fs');
+
+function readFile(path) {
+	fs.createReadStream(path, 'utf8').pipe(concat(function(data) {
+		return data;
+	}));
+}
+
 function readConfig() {
 	const env = process.env.NODE_ENV || 'development';
 
 	if (env == 'development') {
-		
+		return toml.parse(readFile(path.join(__dirname, "../config/test.toml")));
 	}
+	return toml.parse(readFile(path.join(__dirname, "../config/main.toml")));
 }
+
+module.exports = readConfig;
